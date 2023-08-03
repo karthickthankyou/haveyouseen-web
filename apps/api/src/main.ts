8a382cc2c668a479f7fdd01ca0c699c8efb81e65
Karthick Ragavendran <karthickthankyou@gmail.com>
2023-08-03 19:41:35 +0530

    add csurf

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import * as csurf from 'csurf'
 
 const port = process.env.PORT || 3000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(csurf())
 
   const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',')
 
